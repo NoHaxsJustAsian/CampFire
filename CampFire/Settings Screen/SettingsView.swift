174bd261f0f5c05b99615a5719c8a8d49a432c23
Win Tongtawee <WinTongtawee@gmail.com>
2023-06-24 14:21:34 -0400

    Finished Settings View

--- CampFire/Settings Screen/SettingsView.swift
+++ CampFire/Settings Screen/SettingsView.swift
@@ -8,5 +8,54 @@
 import UIKit
 
 class SettingsView: UIView {
-
+    
+    var labelElement: UILabel!
+    var stackView: UIStackView!
+    var notificationSwitch: UISwitch!
+    var notificationTime: UIDatePicker!
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        setupNotificationSettings()
+        initConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //Notification on or off, be sure to prompt user the first time they press it, or if not allowed in settings
+    func setupNotificationSettings() {
+        notificationSwitch = UISwitch()
+        notificationSwitch.isOn = false
+        
+        labelElement = UILabel()
+        labelElement.text = "Notifications"
+        
+        notificationTime = UIDatePicker()
+        notificationTime.datePickerMode = .time
+        self.addSubview(notificationTime)
+        notificationTime.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView = UIStackView(arrangedSubviews: [notificationSwitch, labelElement,notificationTime])
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(stackView)
+    }
+    
+    
+    func initConstraints(){
+        NSLayoutConstraint.activate([
+                // StackView constraints
+                stackView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 20),
+                stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+                stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            ])
+    }
+    
+    
 }
+
--- CampFire/Settings Screen/SettingsViewController.swift
+++ CampFire/Settings Screen/SettingsViewController.swift
@@ -17,7 +17,23 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Settings"
+        settingsView.notificationTime.addTarget(self, action: #selector(notificationTimeValueChanged(_:)), for: .valueChanged)
+        settingsView.notificationSwitch.addTarget(self, action: #selector(notificationSwitchValueChanged(_:)), for: .valueChanged)
     }
 
-
+    @objc func notificationTimeValueChanged(_ sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = .short
+        let selectedTime = dateFormatter.string(from: sender.date)
+        print(selectedTime) //FIXME: change this to store time of notification
+    }
+    
+    @objc func notificationSwitchValueChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            // Enable notifications
+        } else {
+            // Disable notifications
+        }
+    }
 }
