8746fd073b94ff5c0637c0cc4f66e2161e2ce3a6
Tarun Badarvada <31902746+btaruns22@users.noreply.github.com>
2023-06-25 23:58:48 -0400

    Reflection -- table DOW done and popup shows up to reflection page

--- CampFire/Reflection Page/ReflectionView.swift
+++ CampFire/Reflection Page/ReflectionView.swift
@@ -16,5 +16,18 @@ class ReflectionView: UIView {
         // Drawing code
     }
     */
+    var noteTextView: UITextView!
+        
+    var buttonAdd : UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+    
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 
 }
